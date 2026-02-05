6fc277bbe7c9dffa73a350b00fd43b7d266b981c
Prem Jha <premolearn@gmail.com>
2026-02-05 13:54:38 +0530

    render deployment ready

--- app.py
+++ app.py
@@ -44,4 +44,6 @@ def decoder():
     return render_template('decoder.html',name=decryptedMessage)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
